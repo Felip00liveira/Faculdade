1a39be4488abd8fc77c2ba4890242d0fd3629495
Felip00liveira <fonasc100fe@gmail.com>
2025-03-05 06:51:20 -0300

    Delete Beecrowd directory

--- Beecrowd/1008.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-h = int(input())
-vh = float(input())
-s = h * vh
-print('NUMBER =' ,n)
-print(f'SALARY = U$ {s:.2f}')
--- Beecrowd/1014.py
+++
@@ -1,4 +0,0 @@
-X = int(input())
-Y = float(input())
-conta = X/Y
-print(f'{conta:.3f} km/l')
